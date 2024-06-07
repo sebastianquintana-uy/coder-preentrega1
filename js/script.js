c13d08ab44c80e50ad44e41892103141e41c7e2e
sebastianquintana-uy <sebastian@zurweb.com>
2024-06-07 08:53:42 -0300

    agregue mas funciones

--- js/script.js
+++ js/script.js
@@ -1,46 +1,75 @@
-//PreEntrega 1 Coder Js
+// PreEntrega 1 Coder Js
 
-//Declaro las constantes con array de 3 objetos
-
+// Declaro las constantes con array de 3 objetos
 const productos = [
-    {
-      nombre: "Taladro a Bateria",
-      precio: 2900,
-      sku: 222
-    },
-    {
-      nombre: "Sierra Circular",
-      precio: 3500,
-      sku: 333
-    },
-    {
-      nombre: "Destornillador Eléctrico",
-      precio: 1500,
-      sku: 444
-    },
-  ];
+  {
+    nombre: "Taladro a Bateria",
+    precio: 2900,
+    sku: 222
+  },
+  {
+    nombre: "Sierra Circular",
+    precio: 3500,
+    sku: 333
+  },
+  {
+    nombre: "Destornillador Eléctrico",
+    precio: 1500,
+    sku: 444
+  },
+];
 
-//control de ciclos while, find para buscar la info del producto
+// saludo a usuarios
 
+function saludarUsuario() {
+  let nombre = prompt("Por favor, ingresa tu nombre:");
+  if (nombre) {
+    alert("¡Hola, " + nombre + "! Bienvenido/a.");
+  } else {
+    alert("No ingresaste un nombre.");
+  }
+  preguntarPais(); // Llamar a la función país después de saludo
+}
+
+// Pregunto país 
+function preguntarPais() {
+  let pais = prompt("Por favor, ingresa el país donde resides:");
+  if (pais) {
+    alert("Resides en " + pais + ".");
+  } else {
+    alert("No ingresaste un país.");
+  }
+}
+
+// confirmo mensaje
+function confirmarAccion(mensaje) {
+  return confirm(mensaje);
+}
+
+// Lógica principal del programa
+function buscarProducto() {
   let continuar = true;
 
   while (continuar) {
-    const skuIngresado = prompt("Por favor, ingrese el SKU del producto para saber el precio: \n 222 - Taladro a Bateria \n 333 - Sierra Electrica \n 444 - Destornillador Electrico");
+    const skuIngresado = prompt("Por favor, ingrese el SKU del producto para saber el precio: \n 222 - Taladro a Bateria \n 333 - Sierra Circular \n 444 - Destornillador Electrico");
   
     const productoEncontrado = productos.find(producto => producto.sku == skuIngresado);
   
     if (productoEncontrado) {
       confirm(`Producto encontrado: \n Nombre: ${productoEncontrado.nombre} \n Precio: ${productoEncontrado.precio}`);
     } else {
-      alert("Su SKU es incorrecto.");
+      alert("El SKU ingresado es incorrecto.");
     }
   
-    const respuesta = prompt("¿Desea buscar otro SKU? (si/no)");
-    if (respuesta !== 'si') {
+    const respuesta = confirmarAccion("¿Desea buscar otro SKU?");
+    if (!respuesta) {
       continuar = false;
     }
   }
-  
-  alert("Proceso finalizado.");
 
-  
+  alert("Proceso finalizado.");
+}
+
+// ejecuto
+saludarUsuario();
+buscarProducto();
